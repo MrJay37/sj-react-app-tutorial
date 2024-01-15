3124a0ba3ac220470d07a5f21b6653b5c68310cf
Sanket Jain <djsj37@gmail.com>
2024-01-15 18:17:44 -0500

    Addition of content

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+const Home = () => {
+  return <>
+    <h1>Welcome to my website!</h1>
+    <p>Hello, my name is Sanket and I live in Jersey City, NJ</p>
+  </>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  </React.StrictMode>
+  <Home />
 );
 
