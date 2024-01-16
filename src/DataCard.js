277ccbcda1415bf3183f452db7905c5715e2cba9
Chisako Goza <gozachisako@gmail.com>
2024-01-16 16:13:35 -0500

    added props.title and props.body for Exercise 2

---
+++ src/DataCard.js
@@ -0,0 +1,11 @@
+
+const DataCard = props => {
+  return (
+    <div>
+     <h2>  One of my hobbies is{props.title}</h2>  
+   {props.body} 
+      
+    </div>
+  )
+}
+export default DataCard
--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 
 import ReactDOM from 'react-dom/client';
+import DataCard from './DataCard';
+
+const titlesAndBodies = [
+  {title:' cooking', body:'chicken soup,miso soup,ramen'},
+  {title:' flowerArrangement', body:' bouquet,vase arrangement,table decorations'},
+  {title:' yoga' , body:' cobra pose,down dog,camel pose'}
+  
+];
 
 
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Welcome to my website</h1>
-      <p>こんにちは！I'm from Japan, I like delicious dedicated food,flower arrangement and yoga.</p>
-    </div>
-  )
+  return <>
+
+    <h1>Welcome to my website</h1>
+    <p>Hello! I'm Chisako from Japan, I like delicious dedicated food,flower arrangement and yoga.</p>
+   
+    {titlesAndBodies.map((hobby) => 
+    <DataCard title={hobby.title} body={hobby.body}/>)}
+
+  </>
 }
+
+
+
+
+
+
 
 // export default index
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<Home/>
+  <Home />
 );
 
