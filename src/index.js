0030c2e638573a66c5acf4917ba1bd51f9af227a
Chisako Goza <gozachisako@gmail.com>
2024-01-15 19:10:11 -0500

    added h1 and p for the first assignment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
+
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to my website</h1>
+      <p>こんにちは！I'm from Japan, I like delicious dedicated food,flower arrangement and yoga.</p>
+    </div>
+  )
+}
+
+// export default index
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  </React.StrictMode>
+<Home/>
 );
 
